5472a7d117d3f0562364fb07bbeefd08dd40ceed
M-Dearlove <dearlove.matthew@gmail.com>
2025-03-28 23:58:38 -0400

    updated cleanDB.

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,11 +1,13 @@
 import models from '../models/index.js';
 import db from '../config/connection.js';
 
-export default async (modelName: "Question", collectionName: string) => {
+// Define the type for modelName to be more specific
+export default async (modelName: keyof typeof models, collectionName: string) => {
   try {
+    // Now TypeScript knows that modelName is a valid key of models
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
-    }).toArray()
+    }).toArray();
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
