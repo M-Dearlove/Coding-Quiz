2c9c94261a5f28a42785acc7c3450b906fc53b1e
M-Dearlove <dearlove.matthew@gmail.com>
2025-03-29 07:20:44 -0400

    removal of conflicting config files.

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require('cypress');
 const path = require('path');
 
-// Import the Vite config from your existing setup
-// Since this is now a .js file, we'll handle the TypeScript import differently
 let viteConfig;
 try {
-  // Try to load the compiled vite config if available
   viteConfig = require('./client/vite.config').default;
 } catch (e) {
-  // Fallback to a basic configuration if it can't be loaded
   viteConfig = {
     plugins: [],
     server: {
